83dd1e8dda3798d7210dd16bfefb8c7a9fa3766d
Ray-martins1428 <ray.eleoterio@sou.unifeob.edu.br>
2025-10-24 20:46:37 -0300

    Adição da conexão com o banco de dados

--- server/app.js
+++ server/app.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 4040; 
-const dashboardRoutes = require('./routes/routes'); 
-
+require('dotenv').config();
+const db = require('./config/db'); // Conexão com o banco
+const PORT = process.env.PORT || 4040;
+const dashboardRoutes = require('./routes/routes');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'../renderer/views'));
-app.use(express.static(path.join(__dirname,'..', 'public'))); 
-app.use(express.urlencoded({extended: false}));
-
-app.use('/', dashboardRoutes);
-
-
+app.set('views', path.join(__dirname, '../renderer/views'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(dashboardRoutes);
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão com o banco de dados bem-sucedida!');
+});
+
+module.exports = connection;
