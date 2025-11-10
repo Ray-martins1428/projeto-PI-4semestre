114d5351da23ad2392a5a430c95ff9150900cdab
faKonda <f_konda@yahoo.com>
2025-11-10 20:20:53 -0300

    organização dos arquivos DUMP, separando os backups do banco de dados do projeto.

--- server/config/db.js
+++ server/config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
 });
 
 pool.getConnection()
-  .then(() => console.log('Conexão com o banco de dados bem-sucedida!'))
+  .then(() => console.log('Conexao com o banco de dados bem-sucedida!'))
   .catch(err => console.error('Erro ao conectar ao banco:', err));
 
 module.exports = pool;
--- server/routes/api/authRoutes.js
+++ server/routes/api/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      console.error('Erro ao destruir sessão:', err);
+      console.error('Erro ao destruir sessao:', err);
       return res.status(500).json({ error: 'Erro ao deslogar' });
     }
     res.clearCookie('sessid'); // limpa cookie da sessão
