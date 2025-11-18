250e85ca3aa814d7ab2c491a2b8f4b941cb36ba1
Ray-martins1428 <ray.eleoterio@sou.unifeob.edu.br>
2025-11-18 19:24:48 -0300

    Adicionando melhorias no arquivo main para próximas aplicações

--- main.js
+++ main.js
@@ -2,6 +2,26 @@ const { app, BrowserWindow } = require("electron");
 const { spawn } = require("child_process");
 const path = require("path");
 
+let serverProcess = null;
+
+function startServer() {
+  return new Promise((resolve) => {
+    const serverPath = path.join(__dirname, "server", "app.js");
+
+    console.log("Iniciando servidor:", serverPath);
+
+    serverProcess = spawn("node", [serverPath], {
+      stdio: "inherit",
+      shell: true,
+    });
+
+    // Aguarna o servidor subir
+    setTimeout(() => {
+      resolve();
+    }, 1500); // 1.5 seg esperando o Express iniciar
+  });
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1030,
@@ -9,20 +29,18 @@ function createWindow() {
     resizable: false,
   });
 
-  win.loadURL("http://localhost:4040");
+  // Carregar página inicial (Express)
+  win.loadURL("http://localhost:4040/");
   // win.webContents.openDevTools();
 }
 
-app.whenReady().then(() => {
-  // inicia o servidor Node/Express em processo separado
-  const server = spawn("node", [path.join(__dirname, "server", "app.js")], {
-    stdio: "inherit",
-    shell: true,
-  });
-
+app.whenReady().then(async () => {
+  await startServer();   // Aguarda o servidor iniciar
   createWindow();
 
   app.on("will-quit", () => {
-    server.kill();
+    if (serverProcess) {
+      serverProcess.kill();
+    }
   });
 });
